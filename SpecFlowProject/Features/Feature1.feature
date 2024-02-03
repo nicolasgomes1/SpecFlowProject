174a50d5820995b43cd4dbcbffa1324106669c1c
nicolasgomes1 <nicolasdiasgomes@gmail.com>
2024-02-03 09:06:13 +0100

    Add project files.

---
+++ SpecFlowProject/Features/Feature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: Youtube search feature
+
+A short summary of the feature
+
+@TestersTalk
+Scenario: Search for TestersTalk
+	Given Open the browser
+	When Enter the URL
+	Then Search for the Testers Talk
---
+++ SpecFlowProject/StepDefinitions/Class1.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MyNamespace
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private IWebDriver driver;
+
+        public StepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"Open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"Enter the URL")]
+        public void WhenEnterTheURL()
+        {
+            driver.Url = ("https://www.youtube.com/");
+            Thread.Sleep(5000);
+        }
+
+        [Then(@"Search for the Testers Talk")]
+        public void ThenSearchForTheTestersTalk()
+        {
+            IWebElement Element = driver.FindElement(By.XPath(".//*[@name='search_query']"));
+            Element.SendKeys("Testers Talk");
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
